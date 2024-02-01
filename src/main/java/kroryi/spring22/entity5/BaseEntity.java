d2940aa6260cfc237406ad5f84ac927a874040d9
김영맨33 <jangyhun@naver.com>
2024-02-01 15:21:17 +0900

    기본 Entity 사용 시 날짜 자동으로 들어가도록

--- src/main/java/kroryi/spring22/entity5/BaseEntity.java
+++ src/main/java/kroryi/spring22/entity5/BaseEntity.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,8 +20,20 @@ public class BaseEntity {
     @JdbcTypeCode(SqlTypes.INTEGER)
     private Long id;
 
+    @Column(updatable=false)
     private String createdBy;
     private LocalDateTime createdDate;
     private String lastModifiedBy;
     private LocalDateTime lastModifiedDate;
+
+    @PrePersist
+    public void PrePersist(){
+        LocalDateTime now = LocalDateTime.now();
+        createdDate = now;
+        lastModifiedDate = now;
+    }
+    @PreUpdate
+    public void PreUpdate(){
+        lastModifiedDate = LocalDateTime.now();
+    }
 }
